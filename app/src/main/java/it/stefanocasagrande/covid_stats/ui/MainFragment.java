ce2a94d006a0d3f390a008267956495a87ff5d5a
StefanoCasagrande <stefano.casagrande@gmail.com>
2020-12-29 22:08:31 +0100

    Change name of main fragment objects + use strings for titles

--- app/src/main/java/it/stefanocasagrande/covid_stats/ui/MainFragment.java
+++ app/src/main/java/it/stefanocasagrande/covid_stats/ui/MainFragment.java
@@ -23,13 +23,15 @@ import it.stefanocasagrande.covid_stats.json_classes.reports.Total_Response;
  */
 public class MainFragment extends Fragment implements Covid_Interface, View.OnClickListener {
 
-    TextView tv_giorno;
-    TextView tv_casi_confermati;
-    TextView tv_casi_confermati_aumento;
-    TextView tv_morti;
-    TextView tv_morti_aumento;
-    TextView tv_ricoverati;
-    TextView tv_ricoverati_aumento;
+    TextView tv_date;
+    TextView tv_confirmed_cases;
+    TextView tv_confirmed_diff;
+    TextView tv_deaths;
+    TextView tv_deaths_diff;
+    TextView tv_recovered;
+    TextView tv_recovered_diff;
+    TextView tv_active;
+    TextView tv_active_diff;
 
     public MainFragment() {
         // Required empty public constructor
@@ -53,15 +55,17 @@ public class MainFragment extends Fragment implements Covid_Interface, View.OnCl
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_main, container, false);
 
-        tv_giorno = v.findViewById(R.id.tv_giorno);
-        tv_casi_confermati = v.findViewById(R.id.tv_casi_confermati);
-        tv_casi_confermati_aumento = v.findViewById(R.id.tv_casi_confermati_aumento);
-        tv_morti = v.findViewById(R.id.tv_morti);
-        tv_morti_aumento = v.findViewById(R.id.tv_morti_aumento);
-        tv_ricoverati = v.findViewById(R.id.tv_ricoverati);
-        tv_ricoverati_aumento = v.findViewById(R.id.tv_ricoverati_aumento);
+        tv_date = v.findViewById(R.id.tv_date);
+        tv_confirmed_cases = v.findViewById(R.id.tv_confirmed_cases);
+        tv_confirmed_diff = v.findViewById(R.id.tv_confirmed_diff);
+        tv_deaths = v.findViewById(R.id.tv_deaths);
+        tv_deaths_diff = v.findViewById(R.id.tv_deaths_diff);
+        tv_recovered = v.findViewById(R.id.tv_recovered);
+        tv_recovered_diff = v.findViewById(R.id.tv_recovered_diff);
+        tv_active = v.findViewById(R.id.tv_active);
+        tv_active_diff = v.findViewById(R.id.tv_active_diff);
 
-        Button btn_aggiorna = v.findViewById(R.id.btn_aggiorna);
+        Button btn_aggiorna = v.findViewById(R.id.btn_refresh);
         btn_aggiorna.setOnClickListener(this);
 
         ((MainActivity) getActivity()).getTotalReport(this, null);
@@ -72,16 +76,19 @@ public class MainFragment extends Fragment implements Covid_Interface, View.OnCl
     @Override
     public void newReportAvailable(Total_Response wResponse) {
 
-        tv_giorno.setText(wResponse.getData().getdate_dd_MM_yyyy());
+        tv_date.setText(wResponse.getData().getdate_dd_MM_yyyy());
 
-        tv_casi_confermati.setText(String.valueOf(wResponse.getData().getconfirmed()));
-        tv_casi_confermati_aumento.setText(String.valueOf(wResponse.getData().getconfirmed_diff()));
+        tv_confirmed_cases.setText(String.valueOf(wResponse.getData().getconfirmed()));
+        tv_confirmed_diff.setText(String.valueOf(wResponse.getData().getconfirmed_diff()));
 
-        tv_morti.setText(String.valueOf(wResponse.getData().getdeaths()));
-        tv_morti_aumento.setText(String.valueOf(wResponse.getData().getdeaths_diff()));
+        tv_deaths.setText(String.valueOf(wResponse.getData().getdeaths()));
+        tv_deaths_diff.setText(String.valueOf(wResponse.getData().getdeaths_diff()));
 
-        tv_ricoverati.setText(String.valueOf(wResponse.getData().getrecovered()));
-        tv_ricoverati_aumento.setText(String.valueOf(wResponse.getData().getrecovered_diff()));
+        tv_recovered.setText(String.valueOf(wResponse.getData().getrecovered()));
+        tv_recovered_diff.setText(String.valueOf(wResponse.getData().getrecovered_diff()));
+
+        tv_active.setText(String.valueOf(wResponse.getData().getactive()));
+        tv_active_diff.setText(String.valueOf(wResponse.getData().getactive_diff()));
     }
 
     @Override
