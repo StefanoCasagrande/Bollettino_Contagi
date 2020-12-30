f4e2f2be1e65d8c4ffbbf09230a40b7bc5f0a0b5
StefanoCasagrande <stefano.casagrande@gmail.com>
2020-12-30 23:04:16 +0100

    Add newProvinceReportAvailable in Interface

--- app/src/main/java/it/stefanocasagrande/covid_stats/Covid_Interface.java
+++ app/src/main/java/it/stefanocasagrande/covid_stats/Covid_Interface.java
@@ -1,12 +1,12 @@
 package it.stefanocasagrande.covid_stats;
 
 import it.stefanocasagrande.covid_stats.json_classes.regions.Regions;
+import it.stefanocasagrande.covid_stats.json_classes.reports.Province_Response;
 import it.stefanocasagrande.covid_stats.json_classes.reports.Total_Response;
 
 public interface Covid_Interface {
 
     void newReportAvailable(Total_Response wResponse);
 
-    void newNationsAvailable(Regions wResponse);
-
+    void newProvinceReportAvailable(Province_Response wResponse);
 }
